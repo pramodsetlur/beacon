60936c925fca0b2a74fd993495340cf894ba3bb0
Pramod P. Setlur <pramod.setlur@ticketmaster.com>
2016-08-28 01:43:56 -0700

    able to write to db

--- beacon-main/src/main/java/hackathon/slashhack/food/FoodService.java
+++ beacon-main/src/main/java/hackathon/slashhack/food/FoodService.java
@@ -1,13 +1,34 @@
 package hackathon.slashhack.food;
 
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import hackathon.slashhack.food.model.FoodConfig;
 
 /**
  * Created by pramod.setlur on 8/27/16.
  */
 public class FoodService {
+//        DynamoDB dynamoDB = new DynamoDB(new AmazonDynamoDBClient(
+//                new InstanceProfileCredentialsProvider()));
+
+    DynamoDB dynamoDB = new DynamoDB(new AmazonDynamoDBClient(
+            new ProfileCredentialsProvider("shantanu")));
+
+    private Table table = dynamoDB.getTable("EstimoteBeacon");
+
     public String updateDb(FoodConfig foodConfig) {
-        System.out.println(foodConfig.getCategory());
+
+
+
+//        Item item = new Item()
+//                .withPrimaryKey("BeaconCategory", "LunchBruh")
+//                .withString("Store", "subway");
+//        PutItemOutcome outcome = table.putItem(item);
         return "Alright, chill out";
     }
 }
